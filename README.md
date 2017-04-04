dadfe6ba5bc0505001f6082f31f20a272377caa1
David Alexander <thepod@Davids-MacBook-Pro.local>
2017-04-04 15:48:08 -0500

    fill name files

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 Some RPG tools written to practice Ruby
 
 this is very much a WIP, but if somebody stumbles across this and wants anything added, let me know.
+
+to run, navigate to the CoC directory in a terminal and run "ruby CoCcharGen.rb" to auto-generate a character.  You can save the character as a yaml file.
+
+Things I will implement soon:
+loading characters back into the program, for updating
+a combat simulator
+skills
+other text dumps (such as names list, possessions, etc)
+
+things I may some day implement:
+a gui, but very little experience with this so that will probably be a while
+convert a lot of the functionality to yaml so that it can be used with any character creator (the "birth" function for example)
+
+Check issues for known broken areas.
---
+++ scrap/formatter.rb
@@ -0,0 +1,62 @@
+class Formatter
+#this was made to deal with formatting from names taken from book.  was originally in lib
+
+  def spaces(filename)
+    lines = File.readlines("#{Dir.pwd}/#{filename}")
+    lines.each do |line|
+      names = line.split(" ")
+      names.each do |name|
+        puts name
+      end
+    end
+  end
+
+  def nums(filename)
+    males = []
+    females = []
+    surnames = []
+    filestuff = IO.read("#{Dir.pwd}/#{filename}")
+    filearray = filestuff.split(/[\n\r]/)
+    #filearray = filestuff.split("\n")
+
+    filearray.each_with_index do |name,index|
+      if is_i(name)
+        males.push(filearray[index+1])
+        females.push(filearray[index+2])
+        surnames.push(filearray[index+3])
+        index +=1
+      end
+    end
+    dumpstring(males)
+    puts
+    dumpstring(females)
+    puts
+    dumpstring(surnames)
+
+  end
+
+  def dumpstring(arr)
+    for entry in arr
+      puts entry
+    end
+  end
+
+
+  def is_i(thing)
+       /\A[-+]?\d+\z/ === thing
+  end
+end
+
+
+
+form = Formatter.new
+puts Dir.pwd
+puts "space or nums?"
+choice = gets.chomp
+puts "name?"
+filename = gets.chomp
+if choice == "s"
+  form.spaces(filename)
+else
+  form.nums(filename)
+end
