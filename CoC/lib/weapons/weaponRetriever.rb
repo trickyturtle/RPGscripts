69eda66f9696883fe22d3527ab8c520ca95075ac
David Alexander <thepod@Davids-MacBook-Pro.local>
2017-06-27 15:19:50 -0500

    weapon names will always be lowercase

--- CoC/lib/weapons/weaponRetriever.rb
+++ CoC/lib/weapons/weaponRetriever.rb
@@ -2,10 +2,10 @@ require 'yaml'
 
 module WeaponRetriever
 
-  def retrieveWeapon(name)
+  def retrieveWeapon()
     weaponList = YAML.load_file("weaponList.yml")
     puts "What is the weapon name (pistol, shotgun, etc, not its proper name Ian)"
-    name = gets.chomp
+    name = gets.chomp.downcase
     if weaponList.key?(name)
       weapon = weaponList[name]
     else
